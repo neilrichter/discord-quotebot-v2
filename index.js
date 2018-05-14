a610d9d944eb6931cff2087386513a16cdcd314a
SickMyDucks <me@neilrichter.com>
2018-05-14 22:32:22 +0200

    Added french help section

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ client.on('message', message => {
     if (message.content.split(' ')[0] != config.prefix) {
         return;
     }
-
+    
     let splittedMessage = message.content.split(' ');
+
     var options = utils.parseParams(splittedMessage);
     options = utils.extendParams(options, defaults);
+
+    if (splittedMessage[1] == 'help') {
+        let help = require('./help.json')[options.lang];
+        let data = new Discord.RichEmbed(); 
+        data.setThumbnail(client.user.avatarURL);
+        data.setColor(defaults.color);
+        data.setTitle(help.title);
+        data.setDescription(help.requires);
+        data.addField(help.quote.usage, help.quote.desc, false);
+        data.addBlankField(false);
+        data.addField("Options :", `${help.options.desc}\n${help.options.example}`, false);
+        for (var i in help.options.list) {
+            data.addField(`${i.replace(/(^|\s)\S/g, l => l.toUpperCase())} :`, help.options.list[i], false);
+        }
+        data.addBlankField(false);
+        data.addField(help.warning.title, help.warning.desc, false);
+        return message.channel.send(data);
+    }
 
     if (splittedMessage.length == 2) {
         message.channel.fetchMessage(splittedMessage[1])
