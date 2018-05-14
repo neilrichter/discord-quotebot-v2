2d1fe2164044deda5b96a434622554e900602e34
SickMyDucks <me@neilrichter.com>
2018-05-14 19:00:09 +0200

    Can quote from the same channel

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ client.on('message', message => {
     if (message.content.split(' ')[0] != config.prefix) {
         return;
     }
-    message = message.content.split(' ');
-    const options = utils.parseParams(message);
+
+    let splittedMessage = message.content.split(' ');
+    const options = utils.parseParams(splittedMessage);
+
+    if (splittedMessage.length == 2) {
+        message.channel.fetchMessage(splittedMessage[1])
+        .then(quotedMessage => {
+            message.delete();
+            let data = new Discord.RichEmbed();
+            data.setAuthor(`${quotedMessage.author.username} said :`, quotedMessage.author.avatarURL);
+            data.setDescription(quotedMessage.content);
+            data.setFooter('In this channel');
+            data.setTimestamp(typeof quotedMessage.editedTimestamp != 'undefined' ? quotedMessage.editedTimestamp : quotedMessage.createdTimestamp);
+            message.channel.send(data);
+        })
+        .catch(console.error());
+    }
 });
 
 client.on ('ready', () => {
